09a94fb5752c35fabccfe4cd8ead7291afc75fd2
K-KY <kyuyoungk@naver.com>
2025-11-09 20:12:54 +0900

    feat : 사진의 정보를 저장할 리포지토리

---
+++ src/main/java/gamebox/find_same/picture/service/repository/PictureRepository.java
@@ -0,0 +1,44 @@
+package gamebox.find_same.picture.service.repository;
+
+import gamebox.find_same.picture.service.entity.Picture;
+import gamebox.util.exceptions.KeyDuplicatedException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PictureRepository {
+    private final Map<Integer, Picture> pictureMap = new HashMap<>();
+    private static PictureRepository pictureRepository;
+
+    public PictureRepository getInstance() {
+        if (pictureRepository == null) {
+            pictureRepository = new PictureRepository();
+        }
+        return pictureRepository;
+    }
+
+    public Picture save(Picture picture) {
+        try {
+            pictureMap.put(picture.getId(), picture);
+            return picture;
+        } catch (KeyDuplicatedException e) {
+            throw new IllegalArgumentException("[ERROR] 이미 존재하는 아이디 입니다.", e);
+        }
+    }
+
+    public void put(Picture picture) {
+        isIdExist(picture.getId());
+        pictureMap.put(picture.getId(), picture);
+    }
+
+    private void isIdExist(int id) {
+        if (pictureMap.containsKey(id)) {
+            throw new KeyDuplicatedException("[ERROR] Picture with id " + id + " already exists");
+        }
+    }
+
+    public void deleteById(int id) {
+        isIdExist(id);
+        pictureMap.remove(id);
+    }
+}
