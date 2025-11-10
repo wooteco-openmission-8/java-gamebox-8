6ab5f8a022e0f875c7784fc52a045a08b87059cc
kimsz123456 <kimsz123456@gmail.com>
2025-11-10 19:28:06 +0900

    feat: 2048패널 구조 설계

--- src/main/java/gamebox/view/gameScreen/Game2048Panel.java
+++ src/main/java/gamebox/view/gameScreen/Game2048Panel.java
@@ -3,10 +3,31 @@ package gamebox.view.gameScreen;
 import javax.swing.*;
 import java.awt.*;
 
-public class Game2048Panel extends JLabel {
-    public Game2048Panel(){
+public class Game2048Panel extends JPanel {
+    private static final int GRID_SIZE = 4;
+
+    public Game2048Panel() {
+        setupPanel();
+    }
+
+    private void setupPanel() {
         setOpaque(true);
-        setLayout(new BorderLayout());
-        add(new JLabel("Game 2048", SwingConstants.CENTER), BorderLayout.CENTER);
+        setLayout(new GridLayout(GRID_SIZE, GRID_SIZE, 5, 5)); // 5픽셀 간격
+        setBackground(Color.lightGray);
+    }
+
+    private void initBoard() {
+        // TODO: Board(Tile[][]) 초기화
+    }
+
+    public void updateBoard() {
+        // TODO: 전달받은 Board 상태를 패널 내부 변수에 저장
+        repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        // TODO: 내부 Board 상태를 읽어 타일을 화면에 그리기
     }
 }
