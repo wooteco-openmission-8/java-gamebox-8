6659cfd1801c8e999b782e9fd543e4a04b9624f1
kimsz123456 <kimsz123456@gmail.com>
2025-11-11 19:09:51 +0900

    feat: Card클래스 row,col 삭제

--- src/main/java/gamebox/find_same/game/model/Card.java
+++ src/main/java/gamebox/find_same/game/model/Card.java
@@ -1,17 +1,12 @@
 package gamebox.find_same.game.model;
 
 public class Card {
-    private final int id;
     private final int pictureId;
-    private final int row, col;
     private boolean faceUp;
     private boolean matched;
 
-    public Card(int id, int pictureId, int row, int col) {
-        this.id = id;
+    public Card(int pictureId) {
         this.pictureId = pictureId;
-        this.row = row;
-        this.col = col;
     }
 
     public void flip() {
@@ -24,13 +19,7 @@ public class Card {
 
     public void setMatched(){ matched = true; }
 
-    public int getId(){ return id; }
-
     public int getPictureId(){ return pictureId; }
-
-    public int getRow(){ return row; }
-
-    public int getCol(){ return col; }
 
     public boolean isFaceUp(){ return faceUp; }
 
