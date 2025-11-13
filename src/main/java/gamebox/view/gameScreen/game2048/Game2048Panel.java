dbad232032855924af9da42e042948492f123da0
Choi Hansong <gksthd001@g.skku.edu>
2025-11-13 14:47:44 +0900

    feat: 게임 리셋 버튼 구현

--- src/main/java/gamebox/view/gameScreen/game2048/Game2048Panel.java
+++ src/main/java/gamebox/view/gameScreen/game2048/Game2048Panel.java
@@ -4,31 +4,59 @@ import gamebox.game2048.Tile;
 import gamebox.game2048.service.entity.Board;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 public class Game2048Panel extends JPanel {
-    private static final int GRID_GAP = 3;
+    private static final int GRID_GAP = 10;
     private static final int GRID_SIZE = 4;
+
+    private final JPanel resetPanel = new JPanel();
+    private JPanel gamePanel = new JPanel();
 
     private TilePanel[][] tile;
     private Board board;
 
     public Game2048Panel() {
-        setupPanel();
-    }
+        setLayout(new BorderLayout());
+        setFocusable(false);
 
-    private void setupPanel() {
-        setLayout(new GridLayout(GRID_SIZE, GRID_SIZE, GRID_GAP, GRID_GAP));
-        setBackground(Color.white);
+        resetPanel.setBackground(Color.white);
+        JButton resetButton = new JButton("reset");
+        resetButton.setFocusable(false);
+        resetButton.addActionListener(e -> {
+            int reset = JOptionPane.showConfirmDialog(
+                    this,
+                    "게임을 초기화하시겠습니까?",
+                    "확인",
+                    JOptionPane.YES_NO_OPTION
+            );
+            if (reset == JOptionPane.YES_OPTION) {
+                gamePanel.removeAll();
+                initBoard();
+                revalidate();
+                repaint();
+                SwingUtilities.invokeLater(() -> gamePanel.requestFocusInWindow());
+            }
+        });
+        resetPanel.add(resetButton);
+
+        gamePanel.setLayout(new GridLayout(GRID_SIZE, GRID_SIZE, GRID_GAP, GRID_GAP));
+        gamePanel.setBorder(new EmptyBorder(15, 15, 15, 15));
+        gamePanel.setBackground(Color.white);
+
         initBoard();
 
-        // 키 입력이 가능하도록 설정.
-        setFocusable(true);
-        setFocusTraversalKeysEnabled(false);
+        add(resetPanel, BorderLayout.NORTH);
+        add(gamePanel, BorderLayout.CENTER);
 
-        addKeyListener(new KeyAdapter() {
+        // 키 입력이 가능하도록 설정.
+        gamePanel.setFocusable(true);
+        gamePanel.setFocusTraversalKeysEnabled(false);
+
+        gamePanel.addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
                 if (e.getKeyCode() == KeyEvent.VK_UP) {
@@ -59,7 +87,7 @@ public class Game2048Panel extends JPanel {
         });
 
         // 화면이 완전히 보인 뒤 실행하도록.
-        SwingUtilities.invokeLater(this::requestFocusInWindow);
+        SwingUtilities.invokeLater(() -> gamePanel.requestFocusInWindow());
     }
 
     private void initBoard() {
@@ -70,7 +98,7 @@ public class Game2048Panel extends JPanel {
 
     public void updateBoard() {
         // TODO: 전달받은 Board 상태를 패널 내부 변수에 저장
-        removeAll();
+        gamePanel.removeAll();
         drawTiles();
         revalidate();
         repaint();
@@ -82,6 +110,7 @@ public class Game2048Panel extends JPanel {
             for (int c=0; c<GRID_SIZE; c++) {
                 TilePanel currentTile = tile[r][c];
                 currentTile = new TilePanel();
+                currentTile.setFocusable(false);
 
                 /**
                  * 2048 컨트롤러 구현 완료 후 수정
@@ -92,7 +121,7 @@ public class Game2048Panel extends JPanel {
                     currentTile.setColor(tile.getBackgroundColor());
                 }
 
-                add(currentTile);
+                gamePanel.add(currentTile);
             }
         }
     }
